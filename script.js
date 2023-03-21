9d7b6030ad3fab029b68e3060fcf34438bf660ac
Aso200 <asmir_ramiz@hotmail.com>
2023-03-21 13:21:37 +0100

    html

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//Hello World!//
+const nav = document.createElement("nav");
+const header = document.createElement("header");
+const footer = document.createElement("footer");
+const article = document.createElement("article");
+const div = document.createElement("div");
+
+
+document.body.append(nav, header, footer, article, div);
